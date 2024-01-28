1fbf6d53877a2e0a0967b3dd0cf3ca6c0228f0cd
Achinthya630 <achinthya630@gmail.com>
2024-01-28 21:53:53 +0530

    Timer Slider in Homepage, Created Facilities page

--- 1.HOMEPAGE/homepage.js
+++ 1.HOMEPAGE/homepage.js
@@ -17,6 +17,10 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
       clickable: true,
     },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+  },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
---
+++ 6.Facilities/Facilities.js
@@ -0,0 +1,9 @@
+import { getComponent,displayComponent} from "../components.js";
+let comp = getComponent("header");
+displayComponent("homepage-header",comp);
+
+comp = getComponent("Header-Navbar");
+displayComponent("homepage-navbar",comp);
+
+
+// ----------------------------------------
