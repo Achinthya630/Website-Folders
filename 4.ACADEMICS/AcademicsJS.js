adb24d6cee6c4d8fd5f5ef18f257b466af70d9e5
Achinthya630 <achinthya630@gmail.com>
2024-01-29 17:12:14 +0530

    Added Academics

---
+++ 4.ACADEMICS/AcademicsJS.js
@@ -0,0 +1,9 @@
+import { getComponent,displayComponent} from "../components.js";
+let comp = getComponent("Header2");
+displayComponent("homepage-header",comp);
+
+comp = getComponent("Footer");
+displayComponent("Footer",comp);
+//---------------------------------//
+
+
